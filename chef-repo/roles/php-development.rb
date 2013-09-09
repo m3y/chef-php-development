926ad7a3f38259bdfd3f15723e9267be3640ebb8
Masaya Ozawa <ma3ya.ozw+github@gmail.com>
2013-09-10 02:19:16 +0900

    $BMxMQ$9$k(Brole$B$N@0M}(B

--- chef-repo/roles/php-development.rb
+++ chef-repo/roles/php-development.rb
@@ -4,10 +4,26 @@ run_list(
     "recipe[dotdeb]",
     "recipe[dotdeb::php]",
     "recipe[base]",
+    "recipe[base::tmux]",
     "recipe[php]",
     "recipe[php::composer]",
-    "role[database]",
-    "role[webserver]",
+    "recipe[mysql::server]",
+    "recipe[mysql::client]",
+    "recipe[apache2]",
+    "recipe[apache2::mod_rewrite]",
+    "recipe[apache2::mod_deflate]",
+    "recipe[apache2::mod_headers]",
     "recipe[apache2::mod_php5]",
     "recipe[php::mysqlnd]"
+)
+default_attributes(
+    "apache" => {
+        "listen_ports" => ["80", "443"],
+        "docroot_dir" => "/var/www/"
+    },
+    "mysql" => {
+        "server_debian_password" => "",
+        "server_root_password" => "",
+        "server_repl_password" => ""
+    }
 )
